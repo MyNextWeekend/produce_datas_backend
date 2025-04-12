38f34d5b516583c97881a8ddee41ac38aaef140f
orange <47934253+MyNextWeekend@users.noreply.github.com>
2025-04-12 20:48:03 +0800

    feat: 自动创建model

--- scripts/generate_sqlmodel.py
+++ scripts/generate_sqlmodel.py
@@ -1,15 +1,22 @@
 from sqlacodegen.generators import SQLModelGenerator
-from sqlalchemy import MetaData, create_engine
+from sqlalchemy import MetaData
+
+from app.config import settings
+from app.models import engine
 
 
 def get_table_info():
-    engine = create_engine("mysql+pymysql://root:123456@localhost:3306/fms")
     metadata = MetaData()
     generator = SQLModelGenerator(metadata, engine, [])
     metadata.reflect(engine, None, False, None)
     return generator.generate()
 
 
-def write_file(info: str, file: str):
+def write_file(info: str):
+    file = settings.root_dir.joinpath("sqlmodel").joinpath(settings.db_database)
     with open(file, "w") as f:
         f.write(info)
+
+
+if __name__ == '__main__':
+    write_file(get_table_info())
