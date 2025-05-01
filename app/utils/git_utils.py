5a0bc2ae2dcd40e6c186e6bf263bac1329679cb3
MyNextWeekend <jinhu007@outlook.com>
2025-05-01 18:38:32 +0800

    fix: 修改工具类，补充样例

--- app/utils/git_utils.py
+++ app/utils/git_utils.py
@@ -2,99 +2,65 @@ import os
 
 import git
 
+from app.utils.log_utils import Log
 
-def git_clone(git_url, repo_dir):
-    """克隆指定的 Git 仓库到给定目录。
-
-    参数:
-        git_url (str): Git 仓库的 URL。
-        repo_dir (str): 克隆目标目录。
-
-    异常:
-        Exception: 如果克隆失败，将引发异常，并包含错误信息。
-    """
-    if os.path.exists(repo_dir):
-        raise Exception(f"目标目录已存在: {repo_dir}")
-
-    try:
-        git.Repo.clone_from(git_url, repo_dir)
-        print("Git clone success")
-    except git.exc.GitError as e:
-        raise Exception(
-            f"Git clone error: {e} - URL: {git_url}, Target Directory: {repo_dir}"
-        )
+logger = Log().get_logger()
 
 
-def git_pull(repo_dir, remote_name="origin", branch_name="main"):
-    """从远程仓库拉取更改。
+class GitUtil:
 
-    参数:
-        repo_dir (str): 仓库所在目录。
-        remote_name (str): 远程名称（默认为 'origin'）。
-        branch_name (str): 分支名称（默认为 'main'）。
-    """
-    try:
-        repo = git.Repo(repo_dir)
+    def __init__(self, repo_dir: str, git_url: str = None):
+        """
+        :param git_url: 远程地址
+        :param repo_dir: 仓库本地目录
+        """
+        self.repo_dir = repo_dir
+        self.git_url = git_url
+
+    def clone(self):
+        """克隆指定的 Git 仓库到给定目录"""
+        if os.path.isdir(self.repo_dir):
+            logger.error(f"目标目录已存在: {self.repo_dir}")
+            raise RuntimeError(f"目标目录已存在: {self.repo_dir}")
+        if self.git_url is None:
+            logger.error("远程 url 为空")
+            raise RuntimeError("远程 url 为空")
+
+        git.Repo.clone_from(self.git_url, self.repo_dir)
+        logger.info(f"git clone success: from {self.git_url} to {self.repo_dir}")
+
+    def pull(self, remote_name: str = "origin", branch_name: str = "main"):
+        """从远程仓库拉取更改"""
+        repo = git.Repo(self.repo_dir)
         repo.remotes[remote_name].pull(branch_name)
-        print("Changes pulled successfully.")
-    except git.exc.GitError as e:
-        print(f"Error pulling from remote: {e}")
+        logger.info(f"git pull success: {self.repo_dir} - {branch_name}")
 
+    def status(self):
+        """检查指定目录中的 Git 仓库状态"""
+        repo = git.Repo(self.repo_dir)
+        return repo.git.status()
 
-def git_status(repo_dir):
-    """检查指定目录中的 Git 仓库状态。
-
-    参数:
-        repo_dir (str): 仓库所在目录。
-
-    返回:
-        str: 仓库的状态信息。
-    """
-    try:
-        repo = git.Repo(repo_dir)
-        status = repo.git.status()
-        return status
-    except git.exc.GitError as e:
-        raise Exception(f"Git status error: {e} - Repository Directory: {repo_dir}")
-
-
-def git_checkout(repo_dir, branch_name):
-    """切换到指定的分支。
-
-    参数:
-        repo_dir (str): 仓库所在目录。
-        branch_name (str): 要切换的分支名称。
-
-    异常:
-        Exception: 如果切换失败，将引发异常，并包含错误信息。
-    """
-    try:
-        repo = git.Repo(repo_dir)
+    def checkout(self, branch_name):
+        """切换到指定的分支"""
+        repo = git.Repo(self.repo_dir)
         repo.git.checkout(branch_name)
-        print(f"Checked out to branch: {branch_name}")
-    except git.exc.GitError as e:
-        raise Exception(
-            f"Git checkout error: {e} - Repository Directory: {repo_dir}, Branch: {branch_name}"
-        )
+        logger.info(f"git checkout success: {self.repo_dir} - {branch_name}")
 
-
-def git_list_branches(repo_dir):
-    """列出所有分支。
-
-    参数:
-        repo_dir (str): 仓库所在目录。
-
-    返回:
-        list: 分支名称列表。
-    """
-    try:
-        repo = git.Repo(repo_dir)
-        branches = [branch.name for branch in repo.branches]
-        return branches
-    except git.exc.GitError as e:
-        print(f"Error listing branches: {e}")
-        return []
+    def list_branches(self):
+        """列出所有分支"""
+        repo = git.Repo(self.repo_dir)
+        return [branch.name for branch in repo.branches()]
 
 
 if __name__ == "__main__":
-    git_status("/Users/weekend/workSpaces/pycharmProjects/fastApiProject")
+    local_dir = "/Users/weekend/workSpaces/pycharmProjects/fastapi_project/aaa"
+    # 如果配置了 ssh 密钥认证
+    # remote_url = "git@github.com:MyNextWeekend/fastapi_project.git"
+    # 也可以执行提供账号密码(不安全不推荐)
+    # remote_url="https://username:password@github.com/your-username/your-repo.git"
+    # 使用访问令牌通常比使用密码更安全
+    token = "ghp_dajiblVNtaXWhmTT0gVjxxxxxxxxxx"
+    remote_url = f"https://{token}@github.com/MyNextWeekend/fastapi_project.git"
+
+    git_obj = GitUtil(local_dir, remote_url)
+    git_obj.clone()
