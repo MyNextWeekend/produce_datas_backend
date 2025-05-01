7d52f53bca1ad6c837849c7f10ca6ab4eecd613a
MyNextWeekend <jinhu007@outlook.com>
2025-05-01 22:31:55 +0800

    feat: 新增 trace_id 日志追踪

--- app/middleware.py
+++ app/middleware.py
@@ -1,10 +1,11 @@
 import time
+import uuid
 
 from fastapi import FastAPI, Request
-from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.responses import Response
 
-from app.utils.log_utils import Log
+from app.utils.log_utils import Log, trace_id
 
 logger = Log().get_logger()
 
@@ -12,23 +13,29 @@ logger = Log().get_logger()
 class UseTimeMiddleware(BaseHTTPMiddleware):
     """ 计算耗时中间件"""
 
-    def __init__(self, app):
-        super().__init__(app)
-
-    async def dispatch(self, request: Request, call_next) -> Response:
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
         """ 请求耗时 """
         start_time = time.time()
         # 调用下一个中间件或路由处理函数
         result = await call_next(request)
         process_time = time.time() - start_time
-        result.headers["X-Process-Time"] = str(process_time)
+        result.headers["X-Process-Time"] = f"{process_time * 1000:.2f}"
         return result
+
+class TraceIDMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        """请求头获取 trace_id 并设置到上下文中，最终返回用户"""
+        trace_id_header = request.headers.get('X-Trace-Id', str(uuid.uuid4()).replace("-", ""))
+        trace_id.set(trace_id_header)
+        response = await call_next(request)
+        response.headers['X-Trace-Id'] = trace_id_header
+        return response
 
 
 def register_middleware_handle(server: FastAPI):
     logger.info("register middleware handle...")
-    # 添加token验证中间件
-    # server.add_middleware(TokenMiddleware)
+    # 添加 trace_id 中间件
+    server.add_middleware(TraceIDMiddleware)
     # 添加耗时请求中间件
     server.add_middleware(UseTimeMiddleware)
     # 测试
--- app/utils/log_utils.py
+++ app/utils/log_utils.py
@@ -1,8 +1,23 @@
 import logging
+from contextvars import ContextVar
 from logging.handlers import TimedRotatingFileHandler
 
-from ..config import settings
-from .wrapper_utils import singleton
+from app.config import settings
+from app.utils.wrapper_utils import singleton
+
+# 声明上下文变量
+trace_id: ContextVar[str] = ContextVar("trace_id", default="-")
+
+
+class TraceIdFilter(logging.Filter):
+    def filter(self, record):
+        # 将当前上下文中的 trace_id 注入到日志记录中
+        record.trace_id = trace_id.get() or '-'
+        # if "-" == record.trace_id:
+        #     token = str(uuid.uuid4()).replace("-", "")
+        #     trace_id.set(token)
+        #     record.trace_id = token
+        return True
 
 
 @singleton
@@ -10,13 +25,14 @@ class Log:
 
     def __init__(self):
         # 如果已经初始化了就不再执行，避免重复添加handle
-        self.fmt_str = "%(asctime)s【%(levelname)s】-%(filename)s[%(lineno)d]: %(message)s"
+        self.fmt_str = "%(asctime)s【%(levelname)s】-%(filename)s[%(lineno)d][%(trace_id)s]: %(message)s"
         self.logger = logging.getLogger()
         self.logger.setLevel(logging.DEBUG)
         # 添加handle
         self.logger.addHandler(self.console_handle())
         self.logger.addHandler(self.file_handle())
-        self.modify_logger(["uvicorn", "uvicorn.access"])
+        self.logger.addFilter(TraceIdFilter())
+        self.modify_logger(["uvicorn.access", "uvicorn.error"])
 
     def modify_logger(self, names: list[str]):
         """
@@ -30,6 +46,7 @@ class Log:
             logger.handlers.clear()
             logger.addHandler(self.console_handle())
             logger.addHandler(self.file_handle())
+            logger.addFilter(TraceIdFilter())
 
     def file_handle(self) -> logging.Handler:
         """日志文件的handle"""
